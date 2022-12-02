d6eaab3ba9d790f12b74da5c47681949e9e43e53
Steven Gabriel Claros Tapia <cts0028527@est.univalle.edu>
2022-12-01 22:00:25 -0400

    Añadiendo NameEvent

--- ModuleEmployees/Models/Employee.cs
+++ ModuleEmployees/Models/Employee.cs
@@ -10,24 +10,36 @@ namespace ModuleEmployees.Models
         public int EmployeeId { get; set; }
 
         [Required(ErrorMessage = "The Name is required for Employees")]
+        [Range(4, 15, ErrorMessage = "Please enter a value between 4 and 15 characters")]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "The LastName is required for Employees")]
+        [Range(4, 15, ErrorMessage = "Please enter a value between 4 and 15 characters")]
+        [DataType(DataType.Text)]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "The SecondLastName is required for Employees")]
+        [Range(4, 15, ErrorMessage = "Please enter a value between 4 and 15 characters")]
+        [DataType(DataType.Text)]
         public string SecondLastName { get; set; }
 
         [Required(ErrorMessage = "The Phone is required for Employees")]
+        [Range(8, 13, ErrorMessage = "Please enter a value between 8 and 15 characters")]
+        [DataType(DataType.PhoneNumber)]
         public string Phone { get; set; }
 
         [Required(ErrorMessage = "The Address is required for Employees")]
+        [Range(8, 120, ErrorMessage = "Please enter a value between 8 and 120 characters")]
+        [DataType(DataType.Text)]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "The Type is required for Employees")]
         public string Type { get; set; }
 
         [Required(ErrorMessage = "The Ci is required for Employees")]
+        [Range(7, 10, ErrorMessage = "Please enter a value between 7 and 10 characters")]
+        [DataType(DataType.Text)]
         public string Ci { get; set; }
 
         [Required(ErrorMessage = "The Birthday is required")]
